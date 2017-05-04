852bcf773f241d318bfb32b54b7037c511a0c405
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2017-05-04 13:33:05 +0200

    Back to development: 2.9.3+md.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '2.9.3+md.1'
+version = '2.9.3+md.2.dev0'
 
 import os
 from setuptools import setup
